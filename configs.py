039695951989292584877e087b31defc1a0a736f
Aamer Abdul Rahman <ar.aamer@gmail.com>
2021-08-31 09:11:17 +0400

    added isic

--- configs.py
+++ configs.py
@@ -3,13 +3,14 @@ save_dir                    = '/content/logs/'
 miniImageNet_path           	= './data/miniImagenet'
 DTD_path 						= '/content/dtd/images/'
 
-ISIC_path = "./data/ISIC2018"
+ISIC_path = "./data/ISIC2018_Task3_Training_Input"
 ChestX_path = "./data/ChestX-Ray8"
 CropDisease_path = "./data/CropDiseases"
 EuroSAT_path = "./data/EuroSAT/2750"
 
 EuroSAT_unlabelled_path = './data/eurosat_unlabelled_dataset'
 CropDisease_unlabelled_path = './data/plant_disease_unlaballed'
+ISIC_unlabelled_path = './data/unsupervised_isic'
 
 
 num_epochs = 400
